85f82d87dce924235ea6437e440383ee52b497a1
Rayane Ribeiro <46855211+Rayane420@users.noreply.github.com>
2020-09-25 21:11:08 -0300

    att

--- site-empregos/src/main/java/managedBean/PessoaBean.java
+++ site-empregos/src/main/java/managedBean/PessoaBean.java
@@ -3,10 +3,14 @@ package managedBean;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
+import model.UsuarioPessoa;
+
 @ManagedBean (name = "pessoaBean")
 @ViewScoped
 public class PessoaBean {
 
+	UsuarioPessoa usuarioPessoa = new UsuarioPessoa();
+	
 	
 	
 	
